015797c5f6633b6e22d1509617cbfb72bef02589
Prabal Bhandari <prabalb046348@gmail.com>
2022-06-14 21:41:06 +0545

    assets, globals and screens added

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import { StyleSheet,Text,View } from 'react-native-web'
-import { Colors } from 'react-native/Libraries/NewAppScreen'
+import { StyleSheet, Text, View,Dimensions } from 'react-native'
+import HomeScreen from './src/screens/HomeScreen'
 
 const App = () => {
   return (
-    <View>
-      <Text>Yes</Text>
+    <View style = {styles.container}>
+       <HomeScreen />
     </View>
   )
 }
@@ -14,84 +14,9 @@ export default App
 
 const styles = StyleSheet.create({
 
-  container:{
-    flex:1,
-    backgroundColor:Colors.white,
-    paddingBottom:30,
-  },
-  header : {
+container:{
+  flex:1
+}
 
-    backgroundColor : Colors.blue,
-    paddingTop : parameters.statusBarHeight,
-    height : parameters.headerHeight,
-    alignItems : 'flex-start',
-  },
-  
-  image1 : {
-    width : '100',
-    height : '100',
-  },
 
-  image2 : {
-    width : '60',
-    height : '60',
-    borderRadius : '30',
-  },
-
-  home : {
-    backgroundColor : Colors.blue,
-    paddingLeft : '20',
-  },
-
-  text1 : {
-    color : Colors.white,
-    fontSize : 21,
-    paddingBottom : 20,
-    paddingTop : 20
-  },
-
-  text2 : {
-    color : Colors.white,
-    fontSize : 16
-  },
-
-  view1 : {
-    flexDirection : "row",
-    flex : 1,
-    paddingTop : 30,
-  },
-
-  button1 : {
-    height : 40,
-    width : 150,
-    backgroundColor : Colors.black,
-    borderRadius : 20,
-    alignItems : 'center',
-    justifyContent : 'center',
-    marginTop : 30
-  },
-
- button1Text : {
-  color:Colors.white,
-  fontSize : 16,
-  marginTop : -2
- },
-
- card : {
-  alignItems : "center",
-  margin : SCREEN_WIDTH/22
- },
-
- view2 : {
-  marginBottom : 5,
-  borderRadius : 15,
-  backgroundColor : Colors.grey6
-  },
-
-  title:{
-    color : Colors.black,
-    fontSize : 18
-  },
-
-  
- }
+})
